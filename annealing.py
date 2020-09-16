6090be70da8e9645cb7d7f28b73b7c033c359399
kaiser-dan <dkaiser4897@gmail.com>
2020-09-16 17:35:23 -0400

    Initialize Class with Local Moves

--- annealing.py
+++ annealing.py
@@ -26,17 +26,78 @@ __email__ = "kaiserd@iu.edu"
 __status__ = "Development"
 
 # ====================== CLASSES AND FUNCTIONS =======================
-class SANetwork:
+class Annealing:
     '''Class to handle the network data to perform constrained SA on'''
 
-    def __init__(self, fh):
+    def __init__(self, ntwk, number_of_groups):
         '''Constructor for SANetwork class
 
         Inputs : 
-            fh : string
-                fh is a path string to a .gml network file
+            ntwk : networkx Network
+                ntwk is a networkx Network class objct
+            number_of_groups : positive int
+                The number of communities to find
         
         Returns :
             None
         '''
-        self.fh = nx.read_gml(fh)
+        self.ntwk = ntwk
+        self.n = ntwk.number_of_nodes()
+        self.temp = 100
+        self.modularity = 0
+        self.comm = {node: np.random.randint(0,number_of_groups) for node in list(ntwk.nodes())} # two groups for now
+        self.number_of_groups = number_of_groups
+
+    def GlobalMove(self, num_moves = 100):
+        pass
+
+    def LocalMove(self, elligible):
+        node = np.random.choice(elligible) # Take a random node from the community being passed in
+        comm = self.comm # Make a copy to avoid prematurely altering "true" communities
+
+        # Making the local move
+        new_comms = set(comm.values())
+        new_comms.discard(comm[node])
+        comm[node] = np.random.choice(list(new_comms)) 
+
+        # Getting modularity of post-local move partitions
+        Q_comms = [{x for x,y in comm.items() if y == c} for c in set(comm.values())] # formatting for nx function
+        modularity = nx.algorithms.community.modularity(self.ntwk, Q_comms) 
+        
+        # If move is better, adjust the community and modularity accordingly
+        # Alternatively, if move is worse but succeed temperature calculations
+        better = bool(modularity >= self.modularity)
+        temp_move = bool(np.random.rand() <= self.temp) #ADD TEMPERATURE CALCULATION
+        if better or temp_move:
+            self.comm = comm
+            self.modularity = modularity
+            self.temp *= 0.9
+            return True
+        else:
+            self.temp *= 0.9
+            return False
+
+    def helper_LocalMoves(self, num_moves = 100, comm={}, comm_choice = 0):
+        num_moves = (self.n)**2
+        comm = self.comm
+
+        # the set of nodes under a given community 
+        elligible = [node for node, comm in self.comm.items() if comm == comm_choice]
+        
+        # Do the local moves
+        for _ in range(num_moves):
+            val = self.LocalMove(elligible)
+            if val: # check if need to consider the other community now
+                comm_choice += 1
+                comm_choice %= self.number_of_groups
+                elligible = [node for node, comm in self.comm.items() if comm == comm_choice] # redesignate which nodes are elligible
+            else:
+                continue
+
+        print(self.modularity)
+
+
+if __name__ == '__main__':
+    G = nx.read_gml('sourcefile/karate.gml', 'id')
+    Ann = Annealing(G, 2)
+    Ann.helper_LocalMoves(num_moves=Ann.n**2)
